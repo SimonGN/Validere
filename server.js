d57704080e675f9154711831f41a4102f200f48d
diegomendezp <diegomen15@gmail.com>
2019-12-28 21:30:02 +0100

    Contact email working

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const next = require('next');
 const bodyParser = require('body-parser');
+const transport = require('./config/nodemailer.config');
 const nextI18NextMiddleware = require('next-i18next/middleware').default;
 
 const nextI18next = require('./i18n');
@@ -16,8 +17,17 @@ const handle = app.getRequestHandler();
   server.use(bodyParser.json());
 
   server.post('/contacto', (req, res, next) => {
-    console.log(req.body);
-    res.status(200).json({message: 'Email sent'})
+    const { name, email, phone, comentarios } = req.body;
+    transport
+      .sendMail({
+        from: 'Validere <info@validere.es>',
+        to: 'info@validere.es',
+        subject: 'Contacto',
+        text: '',
+        html: `<html><p>Nombre: ${name}</p><p>Email: ${email}</p><p>Teléfono: ${phone}</p><p>Comentarios: ${comentarios}</p></html>`,
+      })
+      .then(() => res.status(200).json({ message: 'Email send' }))
+      .catch(() => res.status(500).json({ message: 'Error Servidor' }));
   });
   server.post('/validate', (req, res, next) => {
     console.log(req.body);
